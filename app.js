7723fa439e75b32b873af695609dad42ac3d486b
cedriclemercier <cedric.lemercier@outlook.com>
2020-06-06 16:54:12 +1000

    Fix_shit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
-const bookingController = require('./routes/bookingController');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 //
